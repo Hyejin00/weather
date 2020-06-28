51150f40d7eae3085a86ceb8a149d53faf465e97
Hyejin00 <svivs137@naver.com>
2020-06-29 00:32:17 +0900

    Add getWeather method

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Loading from './components/Loading';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
+import axios from 'axios';
 
 const API_KEY = "fe72b1e43e617700ae6bc6d19556042e";
 
@@ -9,16 +10,19 @@ export default function App() {
 
 
   const [isLoading, setIsLoading] = useState(true);
-  getWeather = async()=>{
-    
+  getWeather = async(lat, lon)=>{
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+    console.log(data);
   }
   getLocation = async()=>{
     try {
       const res = await Location.requestPermissionsAsync();
       const { coords:{latitude,longitude} } = await Location.getCurrentPositionAsync({});
       console.log(latitude, longitude);
+      getWeather(latitude, longitude);
       setIsLoading(false);
     } catch (error) {
+      console.log(error);
       Alert.alert("Can't find you.", "Set location permissions");
     }
   }
